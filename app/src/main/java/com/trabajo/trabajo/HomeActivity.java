638ee43049834d3ecde5fba6ec0affa769730d3d
phimwrai <phimwrai@gmail.com>
2020-10-19 00:58:11 +0100

    Splash Screen
    Splash Screen Implemented

---
+++ app/src/main/java/com/trabajo/trabajo/HomeActivity.java
@@ -0,0 +1,4 @@
+package com.trabajo.trabajo;
+
+public class HomeActivity {
+}
--- app/src/main/java/com/trabajo/trabajo/SplashScreen.java
+++ app/src/main/java/com/trabajo/trabajo/SplashScreen.java
@@ -6,20 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 5000;
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, SplashScreen.class);
-                startActivity(homeIntent);
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
                 finish();
             }
-        },SPLASH_TIME_OUT);
+        }, 5000);
     }
 }
